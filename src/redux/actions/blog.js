6a622b8aa3f513122627e64c595d817b67a314a4
wanqiuz <zhaotree@126.com>
2018-05-18 19:35:43 +0800

    change the default url to access the data

--- src/redux/actions/blog.js
+++ src/redux/actions/blog.js
@@ -1,16 +1,18 @@
 import { LOAD_ISSUES, LOAD_ISSUE, CHANGE_CURRENT_PAGE, CHANGE_LOGIN_FORM } from '../types/blog';
 
+const ipAndPort = "47.98.208.0:80";
+
 export function loadIssues() {
   return {
     type: LOAD_ISSUES,
     payload: {
-      url: 'http://127.0.0.1:8080/api/v1/articles',
+      url: `http://${ipAndPort}/api/v1/articles`,
     },
   };
 }
 
 export function loadIssue(id) {
-  const url = `http://127.0.0.1:8080/api/v1/articles/${id}`;
+  const url = `http://${ipAndPort}/api/v1/articles/${id}`;
   return {
     type: LOAD_ISSUE,
     payload: {
