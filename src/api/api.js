4c18d6626fe043f267aea2ce5a92ed9ecbe18a4e
wanqiuz <zhaotree@126.com>
2018-08-03 12:40:49 +0800

    add get post function

--- src/api/api.js
+++ src/api/api.js
@@ -23,6 +23,69 @@ export const login = (action) => {
     });
 };
 
+/**
+ * 将对象转成 a=1&b=2的形式
+ * @param object 对象
+ */
+function objectToString(object, array = [], index = 0) {
+  for (let item in object) {
+    array[index] = [item, object[item]];
+  }
+  return new URLSearchParams(arr).toString();
+}
+
+/**
+ * 真正的请求
+ * @param url 请求地址
+ * @param params 请求参数
+ * @param method 请求方式
+ */
+function commonFetch(url, params, method = 'GET', options) {
+  const searchString = objectToString(params);
+  let initObject = {
+    method: method,
+  }
+  if (method === 'GET') {
+    url += '?' + searchString;
+  } else {
+    initObject.body = body;
+  }
+  if (options.cookie != undefined) {
+    initObject.credentials = 'include'; // 强制加入凭据头
+  }
+  if(options.headers != undefined && typeof options.headers == 'object') {
+    initObject.headers = new Headers(options.headers);
+  } else {
+    initObject.headers = new Headers({
+      'Accept': 'application/json', // 通过头指定，获取的数据类型是JSON
+      'Content-Type': 'application/x-www-form-urlencoded',// 指定提交方式为表单提交
+    })
+  }
+  fetch(url, initObject).then(res => {
+    return res.json();
+  }).then(res => {
+    return res;
+  })
+}
+
+/**
+ * GET请求
+ * @param url 请求地址
+ * @param params 请求参数
+ */
+ export function get(url, params, options={}) {
+  return commonFetch(url, params, 'GET', options);
+ }
+
+/**
+ * POST请求
+ * @param url 请求地址
+ * @param params 请求参数
+ */
+export function post(url, params, options={}) {
+  return commonFetch(url, params, 'POST', options);
+}
+
 export const scrollToAnchor = (anchorName) => {
   if (anchorName) {
       let anchorElement = document.getElementById(anchorName);
